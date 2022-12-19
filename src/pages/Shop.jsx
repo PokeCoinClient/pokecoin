f8c3a02f319f1a3038837f5e13e227a14640feaf
lkahl <kahl.lukas@t-online.de>
2022-12-19 11:30:15 +0100

    💩

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,9 +1,43 @@
-import { Heading } from '@chakra-ui/react';
+import { Button, Flex, Heading, Image, Text } from '@chakra-ui/react';
+import { useQuery } from '@tanstack/react-query';
+import axios from '../api/axios';
+import card from '../assets/pokemon-card-backside.jpg';
+
+const getCardPackages = async () => {
+  const resp = await axios.get('/cards/packages');
+  return resp.data;
+};
+
+const getCurrentPackage = async ({ name }) => {
+  const resp = await axios.get(`/cards/packages/${name}`);
+  return resp.data;
+};
+
+const useGetPackages = () => {
+  return useQuery({
+    queryKey: ['cardPackages'],
+    queryFn: getCardPackages,
+  });
+};
 
 function Shop() {
+  const { data: cardPackages } = useGetPackages();
   return (
     <div className="App">
       <Heading>Shop</Heading>
+      <Flex>
+        {cardPackages?.map((currentCard) => {
+          return (
+            <Button
+              key={currentCard}
+              onClick={() => getCurrentPackage({ currentCard })}
+            >
+              {currentCard}
+            </Button>
+          );
+        })}
+        <Image src={card} height="300px" />
+      </Flex>
     </div>
   );
 }
