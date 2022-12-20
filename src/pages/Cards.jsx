4fc0a972567fd161bdd589a59c0bc46169c11c76
lkahl <kahl.lukas@t-online.de>
2022-12-20 12:45:10 +0100

    .

--- src/pages/Cards.jsx
+++ src/pages/Cards.jsx
@@ -1,10 +1,41 @@
-import { Heading } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  Heading,
+  Image,
+  SimpleGrid,
+  Spacer,
+  Text,
+} from '@chakra-ui/react';
+import { useQuery } from '@tanstack/react-query';
+import axios from '../api/axios';
+
+const getCards = async () => {
+  const resp = await axios.get('/cards');
+  return resp.data;
+};
+
+const useGetCards = () => {
+  return useQuery({ queryKey: ['cards'], queryFn: getCards });
+};
 
 function Shop() {
+  const { data: cards } = useGetCards();
+  console.log(cards);
   return (
-    <div className="App">
+    <Box>
       <Heading>Cards</Heading>
-    </div>
+      <SimpleGrid columns={[1, 2, 3]} justifyItems="center">
+        {cards?.cards.map((card) => {
+          return (
+            // eslint-disable-next-line react/jsx-key
+            <Box>
+              <Text>${card}</Text>
+            </Box>
+          );
+        })}
+      </SimpleGrid>
+    </Box>
   );
 }
 
