8f2977e6050f9a531ce463c671a9f6520136d2a5
lkahl <kahl.lukas@t-online.de>
2022-12-20 16:16:06 +0100

    added sear fields

--- src/pages/Cards.jsx
+++ src/pages/Cards.jsx
@@ -1,4 +1,14 @@
-import { Box, Heading, Image, SimpleGrid, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  Heading,
+  Image,
+  Input,
+  InputGroup,
+  InputLeftAddon,
+  SimpleGrid,
+  Text,
+} from '@chakra-ui/react';
 import { useQuery } from '@tanstack/react-query';
 import axios from '../api/axios';
 
@@ -11,16 +21,30 @@ const useGetCards = () => {
   return useQuery({ queryKey: ['cards'], queryFn: getCards });
 };
 
+const viewDetails = () => {
+  return null;
+};
+
 function Shop() {
   const { data: cards } = useGetCards();
   return (
     <Box>
       <Heading>Cards</Heading>
+      <Flex>
+        <InputGroup m="5px">
+          <InputLeftAddon children="Name" />
+          <Input placeholder="Pikachu" />
+        </InputGroup>
+        <InputGroup m="5px">
+          <InputLeftAddon children="Name" />
+          <Input placeholder="Pikachu" />
+        </InputGroup>
+      </Flex>
       <SimpleGrid columns={[1, 2, 3]} justifyItems="center">
         {cards?.cards.map((card) => {
           return (
             <Box key={card.id}>
-              <Image src={card.imageUrl} />
+              <Image src={card.imageUrl} onClick={() => viewDetails(card)} />
               <Text>{JSON.stringify(card.name)}</Text>
             </Box>
           );
