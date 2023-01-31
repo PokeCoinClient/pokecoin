2313a0fde509792f446682c627483e35efdcc70a
Nicolas Moeller <57846375+skyprime17@users.noreply.github.com>
2023-01-31 21:13:10 +0100

    Feature/shop (#7)
    * added buy button to shop but works not yet
    
    * shop modal now works
    
    * 🔥
    
    * :)
    
    * :O
    
    * still need to do shop cards view
    
    * -
    
    * cards view now works and pagination fixed
    
    * - fix merge conflict
    
    * finished
    
    * home
    
    * -
    
    * -
    
    ---------
    
    Co-authored-by: lkahl <kahl.lukas@t-online.de>
    Co-authored-by: Lukas <lukas.kahl@student.hs-rm.de>

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,79 @@
-import { Heading, Box } from '@chakra-ui/react';
+import {
+  Center,
+  Heading,
+  Box,
+  Text,
+  Image,
+  Flex,
+  Spacer,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import icon from '../assets/icon.svg';
+import pokemon from '../assets/pokemon5.png';
+import card from '../assets/pokemon-card-backside.jpg';
+import GroupCards from '../assets/groupPokemon.png';
+
+function Item({ textColor, headerText, bodyText, image, reverse = false }) {
+  return (
+    <Flex
+      h={'22vh'}
+      borderRadius={'10px'}
+      boxShadow={'rgba(0, 0, 0, 0.35) 0px 5px 15px'}
+      flexDirection={reverse ? 'row-reverse' : 'row'}
+    >
+      <Flex padding={5} justifyContent={'center'} flexDirection="column">
+        <Text fontSize="xx-large" textDecor="underline" color={textColor}>
+          {headerText}
+        </Text>
+        <br />
+        <Text fontSize="large" color={textColor}>
+          {bodyText}
+        </Text>
+      </Flex>
+      <Spacer />
+      <Image src={image} height="100%" p={1} />
+    </Flex>
+  );
+}
 
 function Home() {
+  const textColor = useColorModeValue('#554739', '#fff');
   return (
-    <Box>
-      <Heading>Pokecoin</Heading>
-    </Box>
+    <Flex flexDir={'column'} h={'100%'}>
+      <Center gap={5}>
+        <Image src={icon} height="100px" />
+        <Box>
+          <Heading color={textColor}>Pokecoin</Heading>
+          <Text color={textColor}>The best client to mine your pokecoins!</Text>
+        </Box>
+      </Center>
+      <Flex
+        flexDir={'column'}
+        flexGrow={1}
+        justifyContent={'space-evenly'}
+        gap={3}
+      >
+        <Item
+          textColor={textColor}
+          headerText={'Mine Pokecoins!'}
+          bodyText={'Farm Pokecoins with your favorite Pokemon'}
+          image={pokemon}
+        />
+        <Item
+          textColor={textColor}
+          headerText={'Buy Pokemon cards'}
+          bodyText={'Buy super surprising Pokemon card packs'}
+          image={card}
+          reverse={true}
+        />
+        <Item
+          textColor={textColor}
+          headerText={'Collect Pokemon Cards'}
+          bodyText={'Collect rare Pokemon cards and have fun looking at them'}
+          image={GroupCards}
+        />
+      </Flex>
+    </Flex>
   );
 }
 
